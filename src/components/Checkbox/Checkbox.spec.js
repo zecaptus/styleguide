3c0fca4fba9d495adc67686c91989649d5426371
SteevePitis <steeve.pitis@anaplan.com>
2016-08-03 11:45:27 +0200

    More checkbox test

--- src/components/Checkbox/Checkbox.spec.js
+++ src/components/Checkbox/Checkbox.spec.js
@@ -12,4 +12,23 @@ describe('<Checkbox />', () => {
         mountWithTheme(<Checkbox />);
         expect(Checkbox.prototype.componentDidMount.calledOnce).to.equal(true);
     });
+
+    it('Should call onCheck when we toggle the checkbox', () => {
+        const obj = { check: (e) => e.target.checked };
+        sinon.spy(obj, 'check');
+        const wrapper = mountWithTheme(<Checkbox onCheck={obj.check} />);
+
+        wrapper.find('input').simulate('change', { target: { checked: true } });
+        wrapper.find('input').simulate('change', { target: { checked: false } });
+
+        expect(obj.check.calledTwice).to.equal(true);
+        expect(obj.check.firstCall.returned(true)).to.equal(true);
+        expect(obj.check.secondCall.returned(false)).to.equal(true);
+    });
+
+    it('Should be checked with "checked" props', () => {
+        const wrapper = mountWithTheme(<Checkbox checked />);
+
+        expect(wrapper.find('input').node.checked).to.equal(true);
+    });
 });
